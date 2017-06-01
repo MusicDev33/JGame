944a4d20a0e6be205c7761babed5672da15be723
MusicDev33 <smccowan33@gmail.com>
2017-05-31 19:31:50 -0600

    Added files.

---
+++ desktop/src/com/musicdev/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.musicdev.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.musicdev.game.JGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JGame(), config);
+	}
+}
